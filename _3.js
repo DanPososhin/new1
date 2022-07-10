ebd1b236a206cc6ea486dd692311804fc0ca465a
Dan Pososhin <pososhin.dan@gmail.com>
2022-07-10 23:28:00 +0300

    /

---
+++ _3.js
@@ -0,0 +1,14 @@
+function checkAge(age) {
+    if (age > 18) {
+      return true;
+    } else {
+      return confirm('Родители разрешили?');
+    }
+  }
+
+
+  function checkAge2(age) { (age > 18) ? return true : return confirm('Родители разрешили?');}
+
+  
+
+  function checkAge3(age) { (age > 18) ? return true : return confirm('Родители разрешили?');}
