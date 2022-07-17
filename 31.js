b569eed0cd2bc3e9112886585d06082a7c536106
Dan Pososhin <pososhin.dan@gmail.com>
2022-07-17 11:09:23 +0300

    Create 31.js

---
+++ 31.js
@@ -0,0 +1,16 @@
+const myMusic = [
+  {
+    artist: "Billy Joel",
+    title: "Piano Man",
+    release_year: 1973,
+    formats: ["CD", "8T", "LP"],
+    gold: true,
+  },
+  {
+    artist: "Joel",
+    title: "Man",
+    release_year: 1974,
+    formats: ["CD1", "84T", "L5P"],
+    gold: false,
+  },
+];
