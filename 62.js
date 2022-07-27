4580e04b1884aa5ab276d6a342b94119616bd9bd
Dan Pososhin <pososhin.dan@gmail.com>
2022-07-27 21:15:40 +0300

    Update 62.js

--- 62.js
+++ 62.js
@@ -5,3 +5,19 @@ function lowercaseCount(str){
     }
     return count;
   }
+
+
+
+  const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    }
