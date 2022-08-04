dd892884c9b690b383a4031b41a4db116414c6cd
Dan Pososhin <pososhin.dan@gmail.com>
2022-08-04 13:33:13 +0300

    Update 74.js

--- 74.js
+++ 74.js
@@ -1,14 +1,18 @@
-// Определите функцию с именем length, которая принимает список как ввод и возвращает количество элементов в этом списке. 
+// Определите функцию с именем length, которая принимает список как ввод и возвращает количество элементов в этом списке.
 // Вы не должны использовать встроенную функцию length JavaScript.
 //  Например,  length(['a', 'b', 'c', 'd']) должна возвращать 4. Напишите, что происходит на каждом шаге.
 
-
-function length(arr){
-    if (arr[0] === undefined) {
-        return 0;
-    } else {
-        return arr.shift
+const length = function (x) {
+  if (x[0] === undefined) {
+    return 0;
+  } else {
+    let i = 0;
+    for (i = 0; x[0] === undefined; n++) {
+      x.shift();
+      i++;
     }
-}
+    return i;
+  }
+};
 
-console.log(length(['a', 'b', 'c', 'd']));
+console.log(length(["a", "b", "c", "d"]));
